51e67a35148410adfdfa3f35f5871fa1ff8498b8
Jonah Yolles-Murphy <TGTechie01@gmial.com>
2020-11-12 15:17:31 -0500

    success

--- ctrystuff.c
+++ ctrystuff.c
@@ -1,7 +1,26 @@
+#include <stdint.h>
+#include "varad_macro.h"
 
+#define uint32 uint32_t
+#define uint64 uint64_t
 
-int main(void) {
+#define asn(name, val, type) var_##name = val;
+#define asn(name, val, type) var_##name = val;
+#define ret(value, type) type _return_tmp_ = value; goto fn_return_label;
 
-    _fn_return_label:
-        fn_rellocals
-}
+fn(add_two, uint64,
+    args(
+        arg(a, uint32),
+        arg(b, uint32)
+    ),
+    locals(
+        lcl(c, uint64),
+        lcl(fooo, uint64),
+    ),
+    frame(
+        asn(fooo, 5, uint64)
+        asn(c, 5, uint64)
+        asn(b, 5, uint64)
+        ret(get(fooo, uint64), uint64)
+    )
+)
